b740830faffbb2b05241f4298bd38a678c621fa7
SureshDeora <ss9672805637@gmail.com>
2024-12-13 14:33:49 +0530

    Hello World in HTML, JS, REACT

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {id:"head1"}, "Jai Shree Ram");
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
+        console.log(heading);
+       
